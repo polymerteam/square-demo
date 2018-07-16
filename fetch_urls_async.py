5f8e52adac7d88e43829850cc8f434113c1b36d3
David Mora <davidnmora@gmail.com>
2018-07-16 11:48:38 -0400

    Make Polymer Adjustments based on Square data :white_large_square:

--- fetch_urls_async.py
+++ fetch_urls_async.py
@@ -7,15 +7,17 @@ mutex = RLock()
 # GLOBALS
 downloaded_data_array = []
 
+
 # Store data, using a mutex lock to prevent race conditions
 def getAuthorJSON(response, *args, **kwargs):
 	mutex.acquire()
 	try:
 		downloaded_data_array.append(json.loads(response.content))
-		if response.status_code is not 200:
+		status_code = response.status_code
+		if status_code is not 200 and status_code is not 201:  # GET and POST status codes
 			pprint(response.content)
 			raise Exception("ASYNC DOWNLOAD failed with status %d: %s" % (response.status_code, response.url))
-		print "FINISHED DOWNLOADING FROM: " + response.url
+		print "COMPLETED REQUEST TO: " + response.url
 	finally:
 		mutex.release()
 
@@ -25,10 +27,13 @@ def exception_handler(request, exception):
 
 
 # Main function
-def fetch_urls_async(urls_to_fetch, headers):
+def fetch_urls_async(urls_to_fetch, headers, method='GET', data=None):
 	unsent_requests = []
 	for url in urls_to_fetch:
-		unsent_requests.append(grequests.get(url, hooks={'response': getAuthorJSON}, headers=headers))
+		if method == 'GET':
+			unsent_requests.append(grequests.get(url, hooks={'response': getAuthorJSON}, headers=headers))
+		elif method == 'POST':
+			unsent_requests.append(grequests.post(url, hooks={'response': getAuthorJSON}, headers=headers, data=data))
 	# NOTE FOR FUTURE IMPROVEMENTS: Square Connect V1 throws a 429 for size > number_of_requests_at_once.
 	# This is fine if you have urls_to_fetch <= number_of_requests_at_once (each url is a 'locations', in our case).
 	# Dandelion for example, has 8 'locations' on square.
@@ -36,4 +41,3 @@ def fetch_urls_async(urls_to_fetch, headers):
 	# Actually make requests, holds at line until all threads finish:
 	grequests.map(unsent_requests, exception_handler=exception_handler, size=number_of_requests_at_once)
 	return downloaded_data_array
-
--- make_polymer_adjustments.py
+++ make_polymer_adjustments.py
@@ -1,9 +1,13 @@
+from fetch_urls_async import fetch_urls_async
 from pprint import pprint
-
+import json
 
 def make_polymer_adjustments(adjustments):
 	for adjustment in adjustments:
 		pprint(adjustment)
+		adjustment_url = 'https://eszlr18ifi.execute-api.us-west-1.amazonaws.com/staging/ics/v11/adjustments/'
+		headers = {'Content-type' : 'application/json'}
+		fetch_urls_async([adjustment_url], headers, 'POST', json.dumps(adjustment))
 
 
 def update_begin_time(most_recent_time):
