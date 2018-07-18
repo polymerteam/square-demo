7a9065335748701d45ed51a69f4bb869ffd5cb51
David Mora <davidnmora@gmail.com>
2018-07-18 10:47:40 -0400

    Use end_time of previous request as begin_time for next request

--- update_all_team_inventories.py
+++ update_all_team_inventories.py
@@ -16,11 +16,9 @@ last_square_sync_times = get_last_square_sync_times()
 
 for polymer_team, team_data in get_product_mappings_for_all_teams().iteritems():
 	try:
-		last_synced_with_square_at = '2018-03-26T02:50:16Z' #last_square_sync_times[team_data['polymer_team_id']]
-		print('Last synced: ' + last_synced_with_square_at)
-		arrow_object = arrow.get(last_synced_with_square_at)
-		begin_time = arrow_object + timedelta(seconds=1)
-		end_time = '2018-06-29T02:50:16Z'  #'2018-07-18T01:21:21.554316+00:00' #arrow.utcnow()
+		begin_time = last_square_sync_times[team_data['polymer_team_id']]  # '2018-03-27T01:50:16Z'
+		end_time = arrow.utcnow().isoformat() # '2018-03-27T05:50:16Z'  #'2018-07-18T01:21:21.554316+00:00'
+		print('Begin time: ' + begin_time)
 		print('end time:' + str(end_time))
 		print('Requesting inventory data from Square for %s__________________________' % polymer_team)
 		inventory_changes = get_square_changes(begin_time, end_time, team_data['access_token'], team_data['team_products'], team_data['polymer_team_id'])
